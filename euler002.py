2bc258910bf256e7bbe4de6dfb4d02e137c8c2c3
Thiago Pelizoni <tpelizoni@fcl.com.br>
2024-01-17 10:13:07 -0300

    Project Euler 2 problem solved

---
+++ euler002.py
@@ -0,0 +1,17 @@
+def soma_fibonacci_pares(limite):
+    soma = 0
+    a, b = 1, 1
+    while True:
+        a, b = b, a + b
+        if b > limite:
+            break
+        if b % 2 == 0:
+            soma += b
+    return soma
+
+if __name__ == '__main__':
+    t = int(input().strip())
+
+    for _ in range(t):
+        n = int(input().strip())
+        print(soma_fibonacci_pares(n))
