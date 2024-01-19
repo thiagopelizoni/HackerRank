a3d43f90ae2286a723654364053035110e352dd8
Thiago Pelizoni <tpelizoni@fcl.com.br>
2024-01-19 13:05:08 -0300

    Project Euler 98 problem solved with all tests passed

--- euler098.py
+++ euler098.py
@@ -1,33 +1,25 @@
 # https://www.hackerrank.com/contests/projecteuler/challenges/euler098/problem
-# That's my attempt to solve but not passed on all test cases
-from math import sqrt
+from collections import defaultdict
+from math import isqrt
 
-def are_anagrams(num1, num2):
-    return sorted(str(num1)) == sorted(str(num2))
-        
 def largest_square_anagram(N):
-    max_square = int(sqrt(10**N - 1))
-    min_square = int(sqrt(10**(N - 1)))
-    
-    squares = [i**2 for i in range(min_square, max_square+1)]
-    sorted_squares = sorted(squares, key=lambda x: sorted(str(x)))
-    
-    largest_anagram_square = 0
-    current_max_length = 0
+    min_num = 10 ** (N - 1)
+    max_num = 10 ** N
+    min_square = isqrt(min_num)
+    max_square = isqrt(max_num - 1)
+    squares = [i**2 for i in range(min_square, max_square+1) if len(str(i**2)) == N]
 
-    for i in range(len(sorted_squares) - 1):
-        anagram_group = [sorted_squares[i]]
-        while i + 1 < len(sorted_squares) and are_anagrams(sorted_squares[i], sorted_squares[i + 1]):
-            anagram_group.append(sorted_squares[i + 1])
-            i += 1
+    squares_dict = defaultdict(list)
+    for square in squares:
+        key = tuple(sorted(str(square)))
+        squares_dict[key].append(square)
 
-        if len(anagram_group) > current_max_length:
-            current_max_length = len(anagram_group)
-            largest_anagram_square = max(anagram_group)
-        elif len(anagram_group) == current_max_length:
-            largest_anagram_square = max(largest_anagram_square, max(anagram_group))
+    largest_anagram_set = []
+    for key, anagram_set in squares_dict.items():
+        if len(anagram_set) > len(largest_anagram_set):
+            largest_anagram_set = anagram_set
 
-    return largest_anagram_square
+    return max(largest_anagram_set) if largest_anagram_set else None
 
 n = int(input().strip())
 answer = largest_square_anagram(n)
