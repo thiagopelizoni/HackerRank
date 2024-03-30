f0b0114a54bd6f92b09750c02724309ab3fcd170
Thiago Pelizoni <tpelizoni@fcl.com.br>
2024-03-30 14:09:10 -0300

    https://www.hackerrank.com/challenges/manasa-and-stones/problem

---
+++ ProblemSolving/manasa-and-stones.py
@@ -0,0 +1,19 @@
+# https://www.hackerrank.com/challenges/manasa-and-stones/problem
+import os
+
+def stones(n, a, b):
+    return sorted({a * i + b * (n - 1 - i) for i in range(n)})
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+        a = int(input().strip())
+        b = int(input().strip())
+        result = stones(n, a, b)
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+    fptr.close()
