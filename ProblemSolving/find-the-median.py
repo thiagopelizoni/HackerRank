ecda1eae532afc1bc51a9afb81f889bf708e733d
Thiago Pelizoni <tpelizoni@fcl.com.br>
2024-04-09 17:00:03 -0300

    https://www.hackerrank.com/challenges/find-the-median/problem

---
+++ ProblemSolving/find-the-median.py
@@ -0,0 +1,19 @@
+# https://www.hackerrank.com/challenges/find-the-median/problem
+import os
+
+def findMedian(arr):
+    arr.sort()
+    return arr[len(arr) // 2]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = findMedian(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
