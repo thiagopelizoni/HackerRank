bd0acd1a98ba309c4e37c1e54e4359760a4cffe5
Thiago Pelizoni <tpelizoni@fcl.com.br>
2024-04-09 16:33:20 -0300

    https://www.hackerrank.com/challenges/service-lane/problem

---
+++ ProblemSolving/service-lane.py
@@ -0,0 +1,28 @@
+# https://www.hackerrank.com/challenges/service-lane/problem
+import os
+
+def serviceLane(n, cases):
+    return [min(width[i[0]:i[1]+1]) for i in cases]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    t = int(first_multiple_input[1])
+
+    width = list(map(int, input().rstrip().split()))
+
+    cases = []
+
+    for _ in range(t):
+        cases.append(list(map(int, input().rstrip().split())))
+
+    result = serviceLane(n, cases)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
