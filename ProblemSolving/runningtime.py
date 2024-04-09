163ed3af2b9a1bf50cb93ca601e261e6d2a4a276
Thiago Pelizoni <tpelizoni@fcl.com.br>
2024-04-09 16:43:03 -0300

    https://www.hackerrank.com/challenges/runningtime/problem

---
+++ ProblemSolving/runningtime.py
@@ -0,0 +1,25 @@
+# https://www.hackerrank.com/challenges/runningtime/problem
+import os
+
+def runningTime(arr):
+    shifts = 0
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j - 1] > arr[j]:
+            arr[j - 1], arr[j] = arr[j], arr[j - 1]
+            j -= 1
+            shifts += 1
+    return shifts
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = runningTime(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
