f6acbd3269fa31b201487f7ac5d0437834505b7b
Thiago Pelizoni <tpelizoni@fcl.com.br>
2024-04-09 16:00:59 -0300

    https://www.hackerrank.com/challenges/gem-stones/problem

---
+++ ProblemSolving/gem-stones.py
@@ -0,0 +1,22 @@
+# https://www.hackerrank.com/challenges/gem-stones/problem
+import os
+
+def gemstones(arr):
+    return len(set.intersection(*[set(a) for a in arr]))
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = input()
+        arr.append(arr_item)
+
+    result = gemstones(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
