ca859173b0b8a995a482bb9d2e68842d469a61a7
Thiago Pelizoni <tpelizoni@fcl.com.br>
2024-04-09 16:46:34 -0300

    https://www.hackerrank.com/challenges/insertionsort2/problem

---
+++ ProblemSolving/insertionsort2.py
@@ -0,0 +1,17 @@
+# https://www.hackerrank.com/challenges/insertionsort2/problem
+def insertionSort2(n, arr):
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+        print(*arr)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort2(n, arr)
