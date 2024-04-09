2dc2ab6ff0b852838f192e508242de5ea71a459d
Thiago Pelizoni <tpelizoni@fcl.com.br>
2024-04-09 16:49:32 -0300

    https://www.hackerrank.com/challenges/countingsort2/problem

---
+++ ProblemSolving/countingsort2.py
@@ -0,0 +1,28 @@
+# https://www.hackerrank.com/challenges/countingsort2/problem
+import os
+
+def countingSort(arr):
+    count = [0] * 100
+    
+    for num in arr:
+        count[num] += 1
+
+    sorted_arr = []
+    for i in range(len(count)):
+        sorted_arr.extend([i] * count[i])
+
+    return sorted_arr
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
