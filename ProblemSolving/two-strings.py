9e5297942358910c07412b72e6d57817c809fbad
Thiago Pelizoni <tpelizoni@fcl.com.br>
2024-04-19 16:52:26 -0300

    https://www.hackerrank.com/challenges/two-strings/problem

---
+++ ProblemSolving/two-strings.py
@@ -0,0 +1,21 @@
+# https://www.hackerrank.com/challenges/two-strings/problem
+import os
+
+def twoStrings(s1, s2):
+    return "YES" if set(s1) & set(s2) else "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
